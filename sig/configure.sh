5f885350aacdf417a50ede7ee6e4db5279450b2f
Joel Wanner <wannerjo@ethz.ch>
2020-06-23 10:22:12 +0200

    Use static SIG identifier

--- sig/configure.sh
+++ sig/configure.sh
@@ -4,12 +4,11 @@ export ISD=$(ls /etc/scion/gen/ | grep ISD | awk -F 'ISD' '{ print $2 }')
 export AS=$(ls /etc/scion/gen/ISD${ISD}/ | grep AS | awk -F 'AS' '{ print $2 }')
 export IA=${ISD}-${AS}
 export IAd=$(echo $IA | sed 's/_/\:/g')
-export sigID='sigSBAS'
 export sigIP='127.18.0.2'
 
 ASDIR=${SC}/gen/ISD${ISD}/AS${AS}
 SIGDIR=${ASDIR}/sig${IA}-1
-SIGCONF=${SIGDIR}/${sigID}.config
+SIGCONF=${SIGDIR}/sigSBAS.config
 sudo cp sig.config $SIGCONF
 
 # Replace variables in config file
@@ -18,7 +17,7 @@ for var in 'SC' 'LOG' 'ISD' 'AS' 'IA' 'IAd' 'sigID' 'sigIP'; do
 done
 
 # Configure AS topology
-sudo cp ../gen/sig.json ${SIGDIR}/${sigID}.json
+sudo cp ../gen/sig.json ${SIGDIR}/sigSBAS.json
 for topo in ${ASDIR}/*/topology.json; do
     sudo -E python3 gen_topo.py ${topo} ${IA}
 done
