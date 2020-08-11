3d16f86b3c9044697de5c9397a3c4ea5d6ba6a14
Joel Wanner <joel.wanner@inf.ethz.ch>
2020-08-11 13:51:25 +0200

    Adapt SIG configuration to new format

--- sig/configure.sh
+++ sig/configure.sh
@@ -1,11 +1,12 @@
-export SC=/etc/scion
-export LOG=/var/log/scion
-export ISD=$(ls /etc/scion/gen/ | grep ISD | awk -F 'ISD' '{ print $2 }')
-export AS=$(ls /etc/scion/gen/ISD${ISD}/ | grep AS | awk -F 'AS' '{ print $2 }')
-export IA=${ISD}-${AS}
-export IAd=$(echo $IA | sed 's/_/\:/g')
-export sigIP=$(python3 -c "import json; f = open('../nodes.json', 'r'); print(json.load(f)['$SBAS_NODE']['int-sig-ip']); f.close()")
-export sigID='sigSBAS'
+DB=../db.sh
+SC=/etc/scion
+LOG=/var/log/scion
+ISD=$(ls /etc/scion/gen/ | grep ISD | awk -F 'ISD' '{ print $2 }')
+AS=$(ls /etc/scion/gen/ISD${ISD}/ | grep AS | awk -F 'AS' '{ print $2 }')
+IA=${ISD}-${AS}
+IAd=$(echo $IA | sed 's/_/\:/g')
+sigIP=$($DB ${SBAS_NODE} int-sig-ip)
+sigID='sigSBAS'
 
 ASDIR=${SC}/gen/ISD${ISD}/AS${AS}
 SIGDIR=${ASDIR}/sig${IA}-1
@@ -27,9 +28,9 @@ done
 dummyIF='sigdummy'
 sudo ip link add ${dummyIF} type dummy
 sudo ip addr add ${sigIP}/32 brd + dev ${dummyIF} label ${dummyIF}:0
-# TODO: This still assumes that all internal prefixes are from the /16 below.
-#       To avoid this assumption, we need to iterate through all other nodes.
-sudo ip rule add to 172.22.0.0/16 lookup 11 prio 11
+for prefix in $($DB -k int-prefix); do
+    sudo ip rule add to ${prefix} lookup 11 prio 11
+done
 
 # Create SIG service
 SERVICE=/lib/systemd/system/scion-sig@.service
