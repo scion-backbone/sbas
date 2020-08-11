f2e005204adc034ca7d8c24f322e0d9f8fba0e69
Joel Wanner <joel.wanner@inf.ethz.ch>
2020-08-11 15:19:49 +0200

    Fix GRE teardown

--- docker/teardown-gre.sh
+++ docker/teardown-gre.sh
@@ -2,12 +2,13 @@
 DB=../db.sh
 
 for remote in $($DB -r); do
+    dev=sbas${remote}
     ip rule del from all lookup 10 priority 10
-    ip route del $($DB -r ${remote} ext-prefix) dev sbasgre table 10
+    ip route del $($DB -r ${remote} ext-prefix) dev ${dev} table 10
     ip route del $($DB -l ext-prefix) via 10.99.0.3 table 10
 
-    ip link set sbasgre down
+    ip link set ${dev} down
     remote_sig=$($DB -r ${remote} int-sig-ip)
     local_sig=$($DB -l int-sig-ip)
-    ip tunnel del sbasgre mode gre remote ${remote_sig} local ${local_sig} ttl 255
+    ip tunnel del ${dev} mode gre remote ${remote_sig} local ${local_sig} ttl 255
 done
