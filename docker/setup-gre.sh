a69faff2ab36ac4c9095e21f3ec43e5576acf729
Joel Wanner <joel.wanner@inf.ethz.ch>
2020-08-11 15:17:59 +0200

    Fix non-breaking space

--- docker/setup-gre.sh
+++ docker/setup-gre.sh
@@ -3,7 +3,7 @@ DB=../db.sh
 
 for remote in $($DB -r); do
     dev=sbas${remote}
-    remote_sig=$($DB -r ${remote} int-sig-ip)
+    remote_sig=$($DB -r ${remote} int-sig-ip)
     local_sig=$($DB -l int-sig-ip)
     ip tunnel add ${dev} mode gre remote ${remote_sig} local ${local_sig} ttl 255
     ip link set ${dev} up
