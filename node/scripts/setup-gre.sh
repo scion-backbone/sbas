d995fd0742fc284559679642cb2b9812eeb4fc0d
Joel Wanner <joel.wanner@inf.ethz.ch>
2020-11-03 11:12:06 +0100

    Fix bug in GRE scripts

--- node/scripts/setup-gre.sh
+++ node/scripts/setup-gre.sh
@@ -16,6 +16,7 @@ for remote in $($DB -r); do
 
     # Since, in the future, traffic other than just traffic to the remote opt in client might go over the SBAS, this line will be a little more complex.
     ip route add $($DB -r ${remote} ext-prefix) dev ${dev} table 10
-    ip route add $($DB -l ext-prefix) via 10.99.0.3 table 10
-    ip rule add from all lookup 10 priority 10
 done
+
+ip route add $($DB -l ext-prefix) via 10.99.0.3 table 10
+ip rule add from all lookup 10 priority 10
--- node/scripts/teardown-gre.sh
+++ node/scripts/teardown-gre.sh
@@ -7,11 +7,12 @@ if [ -z "$SBAS_NODE" ]; then
     exit
 fi
 
+ip rule del from all lookup 10 priority 10
+ip route del $($DB -l ext-prefix) via 10.99.0.3 table 10
+
 for remote in $($DB -r); do
     dev=sbas-${remote}
-    ip rule del from all lookup 10 priority 10
     ip route del $($DB -r ${remote} ext-prefix) dev ${dev} table 10
-    ip route del $($DB -l ext-prefix) via 10.99.0.3 table 10
 
     ip link set ${dev} down
     remote_sig=$($DB -r ${remote} int-sig-ip)
