92e6ec6eac0c8496df9e4ec1c62c13a719bdfc18
Joel Wanner <joel.wanner@inf.ethz.ch>
2020-11-09 14:32:06 +0100

    Fix missing traffic rules

--- node/sig/configure.sh
+++ node/sig/configure.sh
@@ -4,13 +4,10 @@ SC=/etc/scion
 LOG=/var/log/scion
 sigIP=$($DB -l int-sig-ip)
 
+# Copy services
 SIGCONF=${SC}/sig.toml
 cp sig.toml ${SIGCONF}
-
-# Replace variables in config file
-for var in 'SC' 'LOG' 'ISD' 'AS' 'IA' 'IAd' 'sigID' 'sigIP'; do
-    sudo sed -i "s%\${${var}}%${!var}%g" ${SIGCONF}
-done
+cp ../gen/sig.json ${SC}/sig.json
 
 # Create SIG service
 SERVICE=/lib/systemd/system/scion-sig.service
