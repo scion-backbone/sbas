660a4bc9d28d047116e08f43ca77646f94dd1935
Joel Wanner <joel.wanner@inf.ethz.ch>
2021-01-04 09:30:32 +0100

    Update SIG run script

--- node/sig/run.sh
+++ node/sig/run.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 DB=../scripts/db.sh
 sigIP=$($DB -l int-sig-ip)
-dummyIF='sig'
+sigIF='sig'
 
-# Delete existing configuration (if exists)
-ip link list ${dummyIF} >/dev/null 2>&1
-if [ $? = 0 ]; then
-    ip link del ${dummyIF}
-fi
+systemctl restart scion-sig.service
+
+# Wait for IF to come up
+while ! ip addr show ${sigIF} >/dev/null 2>&1; do
+    sleep 1
+done
+
+# Delete existing rules
 if ! [ -z "$(ip rule list lookup 11)" ]; then
     ip rule del lookup 11
 fi
 
 # Set up IP rules
-ip link add ${dummyIF} type dummy
-ip addr add ${sigIP}/32 brd + dev ${dummyIF} label ${dummyIF}:0
+ip link add ${sigIF} type dummy
+ip addr add ${sigIP}/32 brd + dev ${sigIF} label ${sigIF}:0
 for prefix in $($DB -r int-prefix); do
     ip rule add to ${prefix} lookup 11 prio 11
 done
-
-systemctl restart scion-sig.service
