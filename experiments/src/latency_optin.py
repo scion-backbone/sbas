5870df0b44f8b9f8d31b1c209d7f2e0071b7184e
Joel Wanner <joel.wanner@inf.ethz.ch>
2021-01-29 13:14:09 +0100

    Implement overhead estimation

--- experiments/src/latency_optin.py
+++ experiments/src/latency_optin.py
@@ -1,9 +1,13 @@
 import os
+import sys
 import shutil
 from fabric import Connection
+import csv
+import math
 
 from . import config as cfg
 from .latency_nodes import get_shortest_scion_paths
+from .parser import *
 
 def run(args, data_path):
     customers = cfg.get_customers()
@@ -22,15 +26,18 @@ def run(args, data_path):
     print("done.")
 
     # Parameters
-    ping_flags = "-c 3"
+    ping_flags = "-c 10"
     PING_IP = "ping " + ping_flags
     PING_SCION = "scion ping " + ping_flags
 
-    def dump(conn, cmd, filename):
+    def get_out_path(suffix):
+        return os.path.join(data_path, 'ping_' + suffix + '.' + cfg.OUT_EXT)
+
+    def dump(conn, cmd, suffix):
         res = conn.run(cmd)
         if not res.ok:
             raise Exception
-        with open(os.path.join(data_path, filename + '.' + cfg.OUT_EXT), 'w') as f:
+        with open(get_out_path(suffix), 'w') as f:
             f.write(res.stdout)
 
     def setup(X, conn):
@@ -47,7 +54,7 @@ def run(args, data_path):
     try:
         # Baselines
         print("Recording Internet baseline...")
-        dump(A_conn, f"{PING_IP} {B['public-ip']}", "ping_internet")
+        dump(A_conn, f"{PING_IP} {B['public-ip']}", 'internet')
 
         # Connect customers to SBAS
         print("Connecting to SBAS...", end=' ')
@@ -56,25 +63,47 @@ def run(args, data_path):
         print("done.")
 
         print("Recording ping through SBAS...")
-        dump(A_conn, f"{PING_IP} {B['provider']['addr']}", "ping_sbas")
+        dump(A_conn, f"{PING_IP} {B['provider']['addr']}", 'sbas')
 
         print("Recording SCMP latency...")
         path_specs = get_shortest_scion_paths()
         seq = path_specs[(A['provider']['node'], B['provider']['node'])]
         seq_flag = f"--sequence '{' '.join(seq)}'"
         cmd = f"{PING_SCION} {ISP_B['scion-ia']},0.0.0.0 {seq_flag}"
-        dump(ISP_A_conn, cmd, "ping_scion")
+        dump(ISP_A_conn, cmd, 'nodetonode')
 
         print("Recording latency through tunnels...")
-        dump(A_conn, f"{PING_IP} {ISP_A['ext-vpn-ip']}", "ping_tunnelA")
-        dump(B_conn, f"{PING_IP} {ISP_B['ext-vpn-ip']}", "ping_tunnelB")
+        dump(A_conn, f"{PING_IP} {ISP_A['ext-vpn-ip']}", 'tunnelA')
+        dump(B_conn, f"{PING_IP} {ISP_B['ext-vpn-ip']}", 'tunnelB')
+
     except:
-        # Clean up
         print("ERROR: removing output files")
+        shutil.rmtree(data_path)
+        sys.exit(1)
+    finally:
+        # Clean up connections
         for X, conn in [(A, A_conn), (B, B_conn)]:
             conn.run(f"cd sbas-proto/client && ./stop.sh {X['provider']['node']}")
-        shutil.rmtree(data_path)
 
-def process(args, data_path):
-    pass
+    with open(os.path.join(data_path, 'data.csv'), 'w', newline='') as out:
+        writer = csv.writer(out)
+        writer.writerow(['link', 'avg', 'std'])
+
+        data = {}
+        for key in ['internet', 'sbas', 'tunnelA', 'tunnelB']:
+            data[key] = parse_icmp_ping(get_out_path(key))
+        data['nodetonode'] = parse_scmp_ping(get_out_path('nodetonode'))
+
+        for key, (avg, std) in data.items():
+            writer.writerow([key, avg, std])
+
+        # Estimate overhead
+        total_avg = 0
+        total_var = 0
+        for key in ['tunnelA', 'tunnelB', 'nodetonode']:
+            avg, std = data[key]
+            total_avg += avg
+            total_var += std**2
+
+        writer.writerow(['estimated', total_avg, math.sqrt(total_var)])
 
